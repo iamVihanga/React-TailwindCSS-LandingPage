5ae2bd4d764cbda935b34f71fd4b4251bdd8137c
iamVihanga <vihangarashansilva@gmail.com>
2022-03-25 20:10:57 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import About from "./About";
+import Support from "./Support";
+import AllInOne from "./AllInOne";
+import Pricing from "./Pricing";
+import Footer from "./Footer";
+
+export { Navbar, Hero, About, Support, AllInOne, Pricing, Footer };
